866269b11cac0e63858c7ea6c504c813da8dabc6
gift-alline <gift-alline@gmx.com>
2025-03-05 04:36:17 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double num1 = 5.0;
+    double num2 = 3.0;
+    double result = pow(num1, num2);
+    cout << "The result is: " << result << endl;
+    return 0;
+}
